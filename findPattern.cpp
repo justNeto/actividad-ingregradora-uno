7005f41eebb67320bae31111c8c35424d100a5cb
justNeto <continental.lelgg.2b@gmail.com>
2022-09-11 04:59:10 -0500

    Added file generation utility to program

--- findPattern.cpp
+++ findPattern.cpp
@@ -14,14 +14,18 @@ void prtHelp()
 
 	std::cout << "Finds a certain pattern on a file and points at the index it can be found.\n\n";
 	std::cout << "Use the following options for searching patterns in files.\n";
-      std::cout << "  -f, --file						search this file for the pattern. Can specify multiple files.\n";
+      std::cout << "  -f, --file						search file for the pattern. Can specify multiple files.\n";
       std::cout << "  -p, --pattern						file with the pattern. Can specify multiple patterns.\n";
       std::cout << "  -v, --verbose						prints the program specifics in runtime.\n";
       std::cout << "  -s, --simulate					creates three random files in current directory and searches for two random patterns.\n\n";
 
 	std::cout << "Use the following options for generating test random files and patterns.\n";
-	std::cout << "  -gf, --generate-files					generate a given number of random files of a given length.  They will be named accordingly.\n";
-	std::cout << "  -gp, --generate-patterns				generate a given number of random patterns of a given length. They will be named accordingly.\n\n";
+	std::cout << "  -gf, --generate-files <N> <M>\n";
+	std::cout << "       Generate a number of <N> random files with a length of <M>. They will be named automatically as transmission<0..N>.\n";
+	std::cout << "       If no <M> value selected, default file size of length 10000 will be used.\n\n";
+
+	std::cout << "  -gp, --generate-patterns <N> <M>\n";
+	std::cout << "       Generate a number of <N> random patterns with a length of <M>. They will be automatically as mcode<0...N>.\n\n";
 
 	std::cout << "  -h, --help						shows this help menu and exit.\n";
       std::cout << "  --version						displays current version of script.\n";
@@ -50,6 +54,9 @@ int main(int argc, char ** argv)
 	bool files_inputed = false;
 
 	bool simulation = false; // inputting patterns
+
+	// Some defaults
+	int default_text_size = 10000;
 
 	if (argc == 1) // no opts passed, program was just executed
 	{
@@ -222,14 +229,44 @@ int main(int argc, char ** argv)
 			{
 				std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
 			}
-			else
-			{
-				std::cout << "\n::-Will generate this files: \n";
+		}
 
-				for (auto const &names : gen_files)
-				{
-					std::cout << names << "\n";
-				}
+		// After inputing opts process them
+		if (gen_files.size() > 2)
+		{
+			std::cout << "\n::- Invalid syntax. Try findPattern -h or findPattern --help for more information. \n";
+			exit(1);
+		}
+
+		if (gen_files.size() == 1) // if default is only one, then only number of files selected. Thus, using default size.
+		{
+			// Generate all the files with default value
+			if (verbose) std::cout << "Generating " << gen_files[0] << " files.\n";
+
+			for (int i = 1; i < std::stoi(gen_files[0]) + 1; i++)
+			{
+				std::string aux = "transmission";
+				std::string aux2 = std::to_string(i);
+				std::string result = aux + aux2 + ".txt";
+
+				genText(default_text_size, result);
+			}
+		}
+
+		if (gen_files.size() == 2) // if default is only one, then only number of files selected. Thus, using default size.
+		{
+			// Generate all the files with default value
+			if (verbose) std::cout << "Generating " << gen_files[0] << " files of length " << gen_files[1] << ".\n"; // number of files
+
+			for (int i = 1; i < std::stoi(gen_files[0]) + 1; i++)
+			{
+				std::string aux = "transmission";
+				std::string aux2 = std::to_string(i);
+				std::string result = aux + aux2 + ".txt";
+
+				int aux3 = std::stoi(gen_files[1]);
+
+				genText(aux3, result);
 			}
 		}
 	}
@@ -242,18 +279,9 @@ int main(int argc, char ** argv)
 			{
 				std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
 			}
-			else
-			{
-				std::cout << "\n::-Will use this files to find a solution: \n";
-
-				for (auto const &names : files)
-				{
-					std::cout << names << "\n";
-				}
-			}
-
-
 		}
+
+
 	}
 
 	if (gen_pattern_opt)
