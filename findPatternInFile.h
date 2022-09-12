8e9abbb8547f6ca55a5796d64d68b66d5472cf00
justNeto <continental.lelgg.2b@gmail.com>
2022-09-11 22:21:28 -0500

    Added opts for first, second and third problem separately. Will be using -s to run tests

--- findPatternInFile.h
+++ findPatternInFile.h
@@ -1,3 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
+void solutionFirstProblem(std::vector<std::string> files, std::vector<std::string> patterns)
+{
+	std::cout << "SOLUTION 1\n";
 
+	for (auto &file_name : files)
+	{
+		std::ifstream infile(file_name); // create ifstream var to read from
+		std::string currLine;
+
+		while (std::getline(infile, currLine))
+		{
+			std::cout << "Current line is " << currLine << ".\n";
+		}
+	}
+
+	std::cout << "PRINTING PATTERNS:\n";
+
+	for (auto &file_name : patterns)
+	{
+		std::ifstream infile(file_name); // create ifstream var to read from
+		std::string currLine;
+
+		while (std::getline(infile, currLine))
+		{
+			std::cout << "Current line is " << currLine << ".\n";
+		}
+	}
+}
--- firstProject.cpp
+++ firstProject.cpp
@@ -1,4 +1,7 @@
 #include "genTxtFiles.h"
+#include "findPatternInFile.h" // solution first problem
+#include "palindromeInFile.h" // solution second problem
+#include "commonSubFiles.h" // solution third problem
 #include <vector>
 
 bool verbose;
@@ -24,11 +27,21 @@ void prtHelp()
 
 	std::cout << ":: < First problem > :: \n";
 	std::cout << "Use the following options for searching patterns in files.\n";
-	std::cout << "  -f, --file FILE1, FILE2 ...\n";
+	std::cout << "  -ffp, --files-first-problem FILE1, FILE2 ...\n";
 	std::cout << "       Specify the files that will be used to find a pattern. -f or --file option must be used along this one.\n\n";
 
-	std::cout << "  -p, --pattern	FILE1, FILE2 ...\n";
+	std::cout << "  -pfp, --patterns-first-problem	FILE1, FILE2 ...\n";
 	std::cout << "       Specify the pattern files that will be used. -p or --pattern option must be used along this one.\n\n";
+
+	std::cout << ":: < Second problem > :: \n";
+	std::cout << "Use the following options for detecting malicious palindromes in files.\n";
+	std::cout << "  -fsp --files-second-problem FILE1, FILE2 ...\n";
+	std::cout << "       Specify the files that will be used to find malicious code inside them. \n\n";
+
+	std::cout << ":: < Third problem > :: \n";
+	std::cout << "Use the following options for detecting malicious palindromes in files.\n";
+	std::cout << "  -ftp --files-third-problem FILE1, FILE2 ...\n";
+	std::cout << "       Specify the files that will be used to find malicious code inside them. \n\n";
 
 	std::cout << ":: < Simulation mode > :: \n";
 	std::cout << "  -s, --simulate					Creates three random files in current directory and solves the problems using them.\n\n";
@@ -45,12 +58,16 @@ int main(int argc, char ** argv)
 {
 	// Vectors to save the names of the files and patterns the program will later use
 	std::vector<std::string> files; // existing files
+	std::vector<std::string> files_second; // existing files
+	std::vector<std::string> files_third; // existing files
 	std::vector<std::string> patterns; // existing patterns
 	std::vector<std::string> gen_files; // create files
 	std::vector<std::string> gen_patterns; // create patterns
 
 	// Opts
 	bool file_opt = false;
+	bool files_second_opt = false;
+	bool files_third_opt = false;
 	bool pattern_opt = false;
 	bool gen_file_opt = false;
 	bool gen_pattern_opt = false;
@@ -60,6 +77,8 @@ int main(int argc, char ** argv)
 	bool patterns_generated = false;
 	bool files_generated = false;
 	bool files_inputed = false;
+	bool files_second_inputed = false;
+	bool files_third_inputed = false;
 
 	bool simulation = false; // inputting patterns
 
@@ -78,6 +97,8 @@ int main(int argc, char ** argv)
 	// Loop to get the correct opts for the program
 	while (aux != argc)
 	{
+		/* std::cout << "Current Argv[aux]: " <<argv[aux] << ".\n"; */
+
 		if ((file_opt == true) && (files_inputed == false))  // if file_opt active and files_inputed still false
 		{
 			// New opt in argv[aux] detected
@@ -88,6 +109,36 @@ int main(int argc, char ** argv)
 			}
 
 			files.push_back(argv[aux]);
+			aux++;
+
+			continue;
+		}
+
+		if ((files_second_opt == true) && (files_second_inputed == false))  // if file_second_opt active and files_second_inputed still false
+		{
+			// New opt in argv[aux] detected
+			if (argv[aux][0] == '-')
+			{
+				files_second_inputed = true; // can no longer add into this opt
+				continue;
+			}
+
+			files_second.push_back(argv[aux]);
+			aux++;
+
+			continue;
+		}
+
+		if ((files_third_opt == true) && (files_third_inputed == false))  // if file_third_opt active and files_third_inputed still false
+		{
+			// New opt in argv[aux] detected
+			if (argv[aux][0] == '-')
+			{
+				files_third_inputed = true; // can no longer add into this opt
+				continue;
+			}
+
+			files_third.push_back(argv[aux]);
 			aux++;
 
 			continue;
@@ -151,18 +202,33 @@ int main(int argc, char ** argv)
 			continue;
 	      }
 
-		// Use files to solve the problems
-		if ((argv[aux] == std::string("-f")) || (argv[aux] == std::string("--file")))
+		// Use files to solve first problem
+		if ((argv[aux] == std::string("-ffp")) || (argv[aux] == std::string("--files-first-problem")))
 	      {
 	            file_opt = true;
 			aux++;
 		      continue;
 	      }
 
-		// Use patterns to solve the problems
-	      if ((argv[aux] == std::string("-p")) || (argv[aux] == std::string("--pattern"))) // if file options selected
+		// Use patterns to solve first problem
+	      if ((argv[aux] == std::string("-pfp")) || (argv[aux] == std::string("--patterns-first-problem")))
 	      {
-		      pattern_opt = false; // selects the option to work with
+		      pattern_opt = true; // selects the option to work with
+			aux++;
+		      continue;
+	      }
+
+		// Files for the second problem
+	      if ((argv[aux] == std::string("-fsp")) || (argv[aux] == std::string("--files-second-problem")))
+	      {
+			files_second_opt = true; // selects the option to work with
+			aux++;
+		      continue;
+	      }
+
+	      if ((argv[aux] == std::string("-ftp")) || (argv[aux] == std::string("--files-third-problem")))
+	      {
+			files_third_opt = true; // selects the option to work with
 			aux++;
 		      continue;
 	      }
@@ -209,8 +275,8 @@ int main(int argc, char ** argv)
 		if (verbose) std::cout << "::- Creating files\n";
 
 		// Creating default files
-		genText(20000, "transmission1.txt");
-		genText(20000, "transmission2.txt");
+		genText(100000, "transmission1.txt");
+		genText(100000, "transmission2.txt");
 		genPattern(5, "mcode1.txt");
 		genPattern(5, "mcode2.txt");
 		genPattern(5, "mcode3.txt");
@@ -235,6 +301,8 @@ int main(int argc, char ** argv)
 	// Code to generate random files
 	if (gen_file_opt)
 	{
+		if (verbose) std::cout << "Generating files.\n";
+
 		if (gen_files.empty())
 		{
 			std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
@@ -256,7 +324,8 @@ int main(int argc, char ** argv)
 			}
 		}
 
-		if (gen_files.size() == 2) // if default is only one, then only number of files selected. Thus, using default size.
+		// If default is two, using <N> and <M> options
+		if (gen_files.size() == 2)
 		{
 			// Generate all the files with default value
 			if (verbose) std::cout << "Generating " << gen_files[0] << " files of length " << gen_files[1] << ".\n"; // number of files
@@ -280,7 +349,7 @@ int main(int argc, char ** argv)
 
 	}
 
-	// Code to solve the problems with existing files
+	// Code to validate if files exist
 	if (file_opt)
 	{
 		if (files.empty())
@@ -288,12 +357,93 @@ int main(int argc, char ** argv)
 			std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
 			exit(1);
 		}
+
+		// Else use the files
+		for (auto &file_name : files)
+		{
+			if (verbose) std::cout << "Searching for file : " << file_name << "\n";
+
+			std::ifstream infile(file_name); // create ifstream var to read from
+
+			if(infile.fail())
+			{
+				if (verbose) std::cout << "File does not exist\n";
+				std::cout << "\n::-Inputted '" << file_name << "' file does not exist. Ignoring it and going to the next file.\n\n";
+
+				continue;
+			}
+
+			if (verbose) std::cout << "File does exist\n\n";
+		}
+	}
+
+	// Code to validate if files exist
+	if (files_second_opt)
+	{
+		if (files_second.empty())
+		{
+			std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
+			exit(1);
+		}
+
+		// Else use the files
+		for (auto &file_name : files_second)
+		{
+			if (verbose) std::cout << "Searching for file : " << file_name << "\n";
+
+			std::ifstream infile(file_name); // create ifstream var to read from
+
+			if(infile.fail())
+			{
+				std::cout << "\n::-Error. Inputted '" << file_name << "' file does not exist.";
+				exit(0);
+			}
+
+			if (verbose) std::cout << "File does exist\n\n";
+		}
+	}
+
+	if (files_third_opt)
+	{
+		if ((files_third.empty()) || (files_third.size() == 1))
+		{
+			std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
+			exit(1);
+		}
+
+		if (files_third.size() == 2)
+		{
+			std::ifstream infile(files_third[0]); // create ifstream var to read from
+
+			if(infile.fail())
+			{
+				std::cout << "\n::-Error. Inputted '" << files_third[0] << "' file does not exist.";
+				exit(0);
+			}
+
+			std::ifstream infileTwo(files_third[1]); // create ifstream var to read from
+
+			if(infileTwo.fail())
+			{
+				std::cout << "\n::-Error. Inputted '" << files_third[1] << "' file does not exist.";
+				exit(0);
+			}
+
+			if (verbose) std::cout << "Both files exist\n";
+		}
+
+		if (files_third.size() > 2)
+		{
+			std::cout << "\n::- Invalid syntax. Try findPattern -h or findPattern --help for more information. \n";
+			exit(0);
+		}
+
 	}
 
 	// Code to generate random patterns
 	if (gen_pattern_opt)
 	{
-		std::cout << "Generating patterns\n";
+		if (verbose) std::cout << "Generating patterns.\n";
 
 		// If empty then delete the pattern.
 		if (gen_patterns.empty())
@@ -317,7 +467,7 @@ int main(int argc, char ** argv)
 			}
 		}
 
-		// If default is only one, then only number of files selected. Thus, using default size.
+		// If default is two, using <N> and <M> options
 		if (gen_patterns.size() == 2)
 		{
 			std::cout << "Inside the gen_patterns\n";
@@ -344,24 +494,48 @@ int main(int argc, char ** argv)
 
 	}
 
-	// Code to solve the problems with existing patterns
+	// Code to validate if patterns exist
 	if (pattern_opt)
 	{
-		if (verbose)
+		if (patterns.empty())
 		{
-			if (patterns.empty())
-			{
-				std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
-			}
-			else
-			{
-				std::cout << "\n::-Will use this patterns to find solution: \n";
-
-				for (auto const &names : patterns)
-				{
-					std::cout << names << "\n";
-				}
-			}
+			std::cout << "\n::-Missing arguments. Try findPattern -h or findPattern --help for more information. \n";
+			exit(1);
 		}
+
+		// Else use the patterns
+		for (auto &file_name : patterns)
+		{
+			if (verbose) std::cout << "Searching for pattern : " << file_name << "\n";
+
+			std::ifstream infile(file_name); // create ifstream var to read from
+
+			if(infile.fail())
+			{
+				std::cout << "\n::-Error. Inputted '" << file_name << "' file does not exist.";
+				exit(0);
+			}
+
+			if (verbose) std::cout << "Pattern file does exist\n\n";
+		}
+	}
+
+	// If both patterns and files exist, pass those vectors to function 1.
+	if ((files.size() != 0) && (patterns.size() != 0) && (file_opt) && (pattern_opt)) // if file_opt and pattern_opt selected
+	{
+		// Solve first problem function
+		solutionFirstProblem(files, patterns);
+	}
+
+	if ((files_second.size() != 0) && (files_second_opt)) // if file_opt and pattern_opt selected
+	{
+		// Solve first problem function
+		solutionSecondProblem(files_second);
+	}
+
+	if ((files_third.size() != 0) && (files_third_opt)) // if file_opt and pattern_opt selected
+	{
+		// Solve first problem function
+		solutionThirdProblem(files_third);
 	}
 }
